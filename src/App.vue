689194006bf087527de9fe20c453fb2e0db07364
Jelarry <cadutdutjedd@gmail.com>
2019-01-17 18:54:43 +0800

    adding component communication

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app" class="container">
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div> -->
+    <!-- <router-view/> -->
+    <img class="logo" alt="Vue logo" src="./assets/logo.png">
+    <todo-list></todo-list>
+  </div>
+</template>
+<script>
+import TodoList from './components/TodoList'
+
+export default {
+  name: 'App',
+  components: {
+    TodoList
+  }
+}
+</script>
+
+
+<style>
+
+* {
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+#app {
+  margin-top: 60px;
+}
+
+.logo {
+  display: block;
+  margin: 20px auto;
+  height: 75px;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  font-size: 24px;
+}
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
